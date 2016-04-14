9bff8f5ca1df685eea52694ca35d4a5d22130e4e
Fredrik Christoffer Berg <fredrcb@stud.ntnu.no>
2016-04-14 12:42:36 +0200

    enum

---
+++ core/src/tdt4240/chess/Models/Chessmen/Direction.java
@@ -0,0 +1,8 @@
+package tdt4240.chess.Models.Chessmen;
+
+/**
+ * Created by berg on 14/04/16.
+ */
+public enum Direction {
+    NORTH, NORTHEAST, EAST, SOUTHEAST, SOUTH, SOUTHWEST, WEST, NORTHWEST;
+}
