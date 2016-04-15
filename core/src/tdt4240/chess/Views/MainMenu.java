85491fcdae8f616b423ac1347f6539dba6c96fce
lars <lars_la@live.com>
2016-04-15 15:40:45 +0200

    Improved menu screen

--- core/src/tdt4240/chess/Views/MainMenu.java
+++ core/src/tdt4240/chess/Views/MainMenu.java
@@ -3,60 +3,67 @@ package tdt4240.chess.Views;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.VerticalGroup;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.FitViewport;
-
 import tdt4240.chess.Main;
 
 public class MainMenu implements Screen {
 
     Main game;
-    private final Stage stage = new Stage(new FitViewport(8, 10));
-    Table menu;
+    private final Stage stage = new Stage(new FitViewport(8, 12));
+    
+    VerticalGroup menu;
     TextButton startBtn;
+    TextButton optionsBtn;
     BitmapFont font;
     Skin skin;
     TextureAtlas btnAtlas;
     TextButton.TextButtonStyle btnStyle;
+    Texture backgroundImage;
+    Sprite sprite;
 
     public MainMenu(Main game) {
         this.game = game;
+        backgroundImage = new Texture(Gdx.files.internal("menuScreen.png"));
+        sprite = new Sprite(backgroundImage);
+        sprite.setSize(0.9f, 0.9f* sprite.getHeight() / sprite.getWidth());
     }
 
     public void createMenu() {
-        menu = new Table();
+        menu = new VerticalGroup();
         skin = new Skin();
         font = new BitmapFont();
         btnAtlas = new TextureAtlas(Gdx.files.internal("button.pack"));
         skin.addRegions(btnAtlas);
         btnStyle = new TextButton.TextButtonStyle();
         btnStyle.up = skin.getDrawable("btnUp");
-        btnStyle.up = skin.getDrawable("btnDown");
+        btnStyle.down = skin.getDrawable("btnDown");
         btnStyle.font = font;
+
         startBtn = new TextButton("Start Game", btnStyle);
         startBtn.addListener(new ClickListener() {
             public void clicked(InputEvent event, float x, float y) {
                 game.setScreen(new GameScreen(game));
             }
         });
-        menu.add(startBtn);
+
+        optionsBtn = new TextButton("Options", btnStyle);
+        menu.addActor(startBtn);
+        menu.addActor(optionsBtn);
         menu.setTransform(true);
         menu.setScale(1 / startBtn.getHeight());
-        menu.setPosition(4, 9);
+        menu.setPosition(4, 7);
+        menu.space(10);
         stage.addActor(menu);
-
-
-
-
-
     }
 
 
@@ -70,8 +77,11 @@ public class MainMenu implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.begin();
+        game.batch.draw(backgroundImage, 0, 0, 800, 1280);
+        game.batch.end();
         this.stage.draw();
 
     }
