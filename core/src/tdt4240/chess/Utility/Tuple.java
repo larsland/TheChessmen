9a95b008d711e7da5991f85e3cea4166bb0e9369
Fredrik Christoffer Berg <fredrcb@stud.ntnu.no>
2016-04-17 15:36:12 +0200

    asd

--- core/src/tdt4240/chess/Utility/Tuple.java
+++ core/src/tdt4240/chess/Utility/Tuple.java
@@ -10,7 +10,6 @@ public class Tuple {
     }
 
     public boolean equals(Tuple other) {
-        System.out.println("Triggered");
         return (other.getX() == this.x && other.getY() == this.y);
     }
 
@@ -23,4 +22,5 @@ public class Tuple {
     public void setY(int y) {
         this.y = y;
     }
+    public void setX(int x) { this.x = x; }
 }
